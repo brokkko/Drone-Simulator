c6821c94192daebc2ded27f3e68f0c082c60d7d4
techi9 <tucha.989@gmail.com>
2022-04-15 23:20:31 +0300

    docker

---
+++ simContainer/sim/start.sh
@@ -0,0 +1,5 @@
+./simulator-cli UDP model 3 lat 59.9921526 lon 30.2805271 enable-magnetometer 1 &
+
+./Autopilot --sim-udp 127.0.0.1 5002 5000 --modem-tcp 127.0.0.1:8000 --camera --type 425 --root ./root --plazlink-1v6 &
+
+wait

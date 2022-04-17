acfc18741489049c6bd96908254d49c5cc6a365c
techi9 <tucha.989@gmail.com>
2022-04-18 01:07:54 +0300

    fix config

--- simContainer/sim/start.sh
+++ simContainer/sim/start.sh
@@ -1,6 +1,6 @@
 ./simulator-cli UDP model 3 lat 59.9921526 lon 30.2805271 enable-magnetometer 1 &
 
-./Autopilot --sim-udp 127.0.0.1 5002 5000 --modem-tcp 127.0.0.1:8000 --camera --type 425 --root ./cache --plazlink-1v6 &
+./Autopilot --sim-udp 127.0.0.1 5002 5000 --modem-tcp 127.0.0.1:8000 --camera --type 425 --root ./cache --plazlink-1v6 & #TODO: resolve like in test
 
 wait
 
--- simContainer/sim/start_test.sh
+++ simContainer/sim/start_test.sh
@@ -1,4 +1,4 @@
-./Autopilot --sim-udp 127.0.0.1 5002 5000 --modem-tcp 127.0.0.1:8000 --camera --type 425 --root ./cache --plazlink-1v6 &
+./Autopilot --sim-udp 127.0.0.1 5002 5000 --modem-tcp 172.17.0.3:8000 --camera --type 425 --root ./cache --plazlink-1v6 & # TODO: resolve ip using bash
 
 ./simulator model 3 lat 59.9921526 lon 30.2805271 enable-magnetometer 1 udp-in-port 5002 udp-out-port 5000 &
 

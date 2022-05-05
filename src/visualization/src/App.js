7a40fcf1134394290d6b4deead30557ab9b0b158
brokkko <nsstrannikova@mail.ru>
2022-05-05 17:23:38 +0300

    added connection button

--- src/visualization/src/App.js
+++ src/visualization/src/App.js
@@ -5,23 +5,13 @@ import {useEffect, useState} from "react";
 function App() {
 
 
-  let socket
 
-  const [state, setState] = useState('no')
-
-
-  socket = new WebSocket("ws://127.0.0.1:8765");
-
-  socket.onopen = function(e) {
-    setState('connected')
-  };
 
 
 
   return (
     <div className="App">
-      <Scene Socket = {socket}/>
-      <h1>{state}</h1>
+      <Scene/>
     </div>
   );
 }

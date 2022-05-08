bc42a81b3299d05292699c5b495514e188d30fd8
techi9 <tucha.989@gmail.com>
2022-05-08 17:55:26 +0300

    DockerService.py works

--- src/DockerService/DockerService.py
+++ src/DockerService/DockerService.py
@@ -1,4 +1,5 @@
 import docker
+
 import os
 import configparser
 
@@ -14,12 +15,23 @@ def runDocker(latlonList: List[tuple]) -> None:
     c.read("config.ini")
     config = c['docker']
 
+    image = config['image_name']
+    client = docker.from_env()
+
     # --env LAT=59.99234113142696 --env LON=30.280999708037204
     for i, latlon in enumerate(latlonList):
-        os.system(f"docker create -p {int(config['start_port']) + i}:8000"
-                  f" -v {config['path'] + str(i)}:/usr/src/app/cache"
-                  f" --env LAT={latlon[0]} --env LON={latlon[1]}"
-                  f" --name sim{i} --rm sim ./start.sh")
+        # os.system(f"docker run -p {int(config['start_port']) + i}:8000"
+        #           f" -v {config['path'] + str(i)}:/usr/src/app/cache"
+        #           f" --env LAT={latlon[0]} --env LON={latlon[1]}"
+        #           f" --name sim{i} -it --rm sim ./start.sh")
+        # print('hi')
+
+        port = {'8000/tcp': int(config['start_port']) + i}  # container: host
+        mount = {config['path'] + str(i): {'bind': '/usr/src/app/cache', 'mode': 'rw'}} # host : container
+        env = [f'LAT={latlon[0]}', f'LON={latlon[1]}']
+        name = image + str(i)
+        client.containers.run(image, "./start.sh", ports=port, volumes=mount, environment=env, tty=True, auto_remove=True, name=name, detach= True)
+        print('hi')
 
 
 if __name__ == '__main__':
